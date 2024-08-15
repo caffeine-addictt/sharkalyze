8c1bb799ef774c75decb9730d7cbe8a2a18df491
AlexNg <contact@ngjx.org>
2024-08-16 01:33:55 +0800

    feat: Add route-map

---
+++ client/src/pages/route-map.tsx
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+import * as React from 'react';
+
+// Page imports
+import NotFound from '@pages/404';
+import RootPage from '@pages/root';
+
+export type PathStr = `/${string}`;
+export type RootPathStr = '*' | '/' | PathStr;
+export type PageComponent = (
+  props: React.ComponentProps<'div'>,
+) => React.JSX.Element;
+
+export type RouteDetails = {
+  component: PageComponent;
+  title: string;
+  description?: string;
+};
+export type RouteMap = Record<PathStr, RouteDetails>;
+export type RootRouteMap = Record<RootPathStr, RouteDetails>;
+
+const routes: RootRouteMap = {
+  '*': { component: NotFound, title: 'Page Not Found' },
+  '/': { component: RootPage, title: 'Home' },
+} as const;
+export default routes;
