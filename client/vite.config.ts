449eef0e6dba98071ea1351fadda4cc2de9625f1
AlexNg <contact@ngjx.org>
2024-08-15 23:33:57 +0800

    feat: Add client/ '@' aliasing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tsconfigPaths(), react()],
+});
