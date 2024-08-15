92c5c824690910575b018b3b5b926c7b410ec73c
AlexNg <contact@ngjx.org>
2024-08-16 01:34:03 +0800

    feat: Implement main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,69 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+/**
+ * SPDX-FileCopyrightText: 2024 Ng Jun Xiang <contact@ngjx.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import * as React from 'react';
+
+import ReactDOM from 'react-dom/client';
+import { useLocation, Route, Routes, BrowserRouter } from 'react-router-dom';
 import '@styles/globals.css';
+
+import { Helmet } from 'react-helmet';
+import routes, { type RouteDetails } from '@pages/route-map';
+
+// Components
+import Navbar from '@components/navbar';
+import Footer from '@components/footer';
+
+export const WrappedComponent = ({
+  component: Component,
+  path,
+  title,
+  description,
+}: RouteDetails & { path: string }): JSX.Element | null => {
+  return (
+    <>
+      <Helmet titleTemplate={path !== '/' ? '%s | GreenBitesSG' : '%s'}>
+        <title>{title}</title>
+        <meta name="description" content={description ?? 'Sharkalyze'} />
+      </Helmet>
+      <Component className="flex w-full max-w-full grow" />
+    </>
+  );
+};
+
+export const Layout = (): JSX.Element => {
+  const location = useLocation();
+
+  return (
+    <div className="dark:bg-background-dark min-w-full max-w-full flex-col flex bg-background-light text-text-light  dark:text-text-dark">
+      <main className="flex min-h-screen flex-col">
+        <Navbar />
+
+        <Routes location={location}>
+          {Object.entries(routes).map(([path, details], i) => (
+            <Route
+              key={i}
+              path={path}
+              element={
+                <WrappedComponent {...details} path={location.pathname} />
+              }
+            />
+          ))}
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
