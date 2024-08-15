2f61a4d84a8edc229c3866ff1f189619e064f1d3
AlexNg <contact@ngjx.org>
2024-08-16 01:19:14 +0800

    feat: Add postcss and tailwind configs

---
+++ client/tailwind.config.ts
@@ -0,0 +1,65 @@
+import type { Config } from 'tailwindcss';
+import anim from 'tailwindcss-animate';
+
+const config = {
+  content: ['./src/**/*.{ts,tsx}'],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        background: {
+          light: '#F1FEF4',
+          dark: '#2A2928',
+        },
+        surface: {
+          light: '#F8F8F8',
+          dark: '#202020',
+        },
+        primary: {
+          light: '#3CC360',
+          dark: {
+            DEFAULT: '#CDBCB1',
+            200: '#B5A69C',
+          },
+        },
+        text: {
+          light: '#010E04',
+          dark: '#EBE5E0',
+        },
+        secondary: {
+          light: '#7EAAF7',
+          dark: '#5ACA41',
+        },
+        accent: {
+          light: '#5552F4',
+          dark: '#73A586',
+        },
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [anim],
+} satisfies Config;
+
+export default config;
