34fbb0556b4af4799217079408df82a517ab1d22
AndrewAce <andrewkangmingyi@gmail.com>
2024-08-18 21:08:56 +0800

    feat: Added qr route to root route-map

--- client/src/pages/route-map.tsx
+++ client/src/pages/route-map.tsx
@@ -9,6 +9,7 @@ import * as React from 'react';
 // Page imports
 import NotFound from '@pages/404';
 import RootPage from '@pages/root';
+import qrRouteMap from './qr/route-map';
 
 export type PathStr = `/${string}`;
 export type RootPathStr = '*' | '/' | PathStr;
@@ -27,5 +28,6 @@ export type RootRouteMap = Record<RootPathStr, RouteDetails>;
 const routes: RootRouteMap = {
   '*': { component: NotFound, title: 'Page Not Found' },
   '/': { component: RootPage, title: 'Home' },
+  ...qrRouteMap,
 } as const;
 export default routes;
