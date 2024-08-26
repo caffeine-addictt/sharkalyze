093c126586b84ff4cfeac633292e9cdc572d08e8
KenNuYu <tanjy0206@gmail.com>
2024-08-27 00:07:03 +0800

    fix: convention for import routemap

--- client/src/pages/route-map.tsx
+++ client/src/pages/route-map.tsx
@@ -9,7 +9,7 @@ import * as React from 'react';
 // Page imports
 import NotFound from '@pages/404';
 import RootPage from '@pages/root';
-import qrRouteMap from './qr/route-map';
+import qrRouteMap from '@pages/qr/route-map';
 
 export type PathStr = `/${string}`;
 export type RootPathStr = '*' | '/' | PathStr;
